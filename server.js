fe6b342fd0d0d188ff67e2276841e291c3662862
yyeryy <yerayaller@gmail.com>
2024-02-07 18:07:54 +0100

    bcrypt

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ app.disable("x-powered-by");
 var fs = require("fs");
 var path = require("path");
 
-//Añadir hash blowfish
-var bcrypt = require('bcrypt');
-
 app.use(function (req, res, next) {
   res.set({
     "Access-Control-Allow-Origin": "*",
